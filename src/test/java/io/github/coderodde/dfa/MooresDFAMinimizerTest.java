bbfe0a3f3eaa4a87d4bb21397dcb97795e2e8661
Rodion Efremov <coderodd3@yandex.com>
2026-03-01 13:03:45 +0200

    Moore's algorithm passes a unit test.

---
+++ src/test/java/io/github/coderodde/dfa/MooresDFAMinimizerTest.java
@@ -0,0 +1,50 @@
+package io.github.coderodde.dfa;
+
+import java.util.Set;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MooresDFAMinimizerTest {
+
+    @Test
+    public void minimize() {
+        TransitionFunction tf = new TransitionFunction();
+        
+        tf.addStateTransition(0, 1, '0');
+        tf.addStateTransition(0, 1, '1');
+        tf.addStateTransition(1, 0, '0');
+        tf.addStateTransition(1, 0, '1');
+        
+        tf.addStateTransition(2, 3, '0');
+        tf.addStateTransition(2, 3, '1');
+        tf.addStateTransition(3, 2, '0');
+        tf.addStateTransition(3, 2, '1');
+        
+        DFA dfa = new DFA(tf, 0, Set.of(0, 2));
+        
+        assertEquals(4, dfa.getTransitionFunction().getAllStates().size());
+        
+        DFA minDfa = new MooresDFAMinimizer().minimize(dfa);
+        
+        assertEquals(2, minDfa.getTransitionFunction().getAllStates().size());
+        
+        assertFalse(dfa.matches("0"));
+        assertFalse(minDfa.matches("0"));
+        
+        assertTrue(dfa.matches("00"));
+        assertTrue(minDfa.matches("00"));
+        
+        assertTrue(dfa.matches("10"));
+        assertTrue(minDfa.matches("10"));
+        
+        assertTrue(dfa.matches("0101"));
+        assertTrue(minDfa.matches("0101"));
+    
+        assertFalse(dfa.matches("110"));
+        assertFalse(minDfa.matches("110"));
+    }
+}
